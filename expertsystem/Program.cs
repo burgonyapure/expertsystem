1db50aca2ed8294211269ef014932261580ae366
burgonyapure <deejay.spory@gmail.com>
2020-02-03 10:03:05 +0100

    Init

---
+++ expertsystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace expertsystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
