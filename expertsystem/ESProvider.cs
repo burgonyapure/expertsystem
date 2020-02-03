f49a69397937dfd7d96273361b6b8d9dabe426bd
burgonyapure <deejay.spory@gmail.com>
2020-02-03 14:14:19 +0100

    asd

--- expertsystem/ESProvider.cs
+++ expertsystem/ESProvider.cs
@@ -6,12 +6,25 @@ namespace expertsystem
 {
     class ESProvider
     {
-        FactParser fact = new FactParser();
-        RuleParser rule = new RuleParser();
+        private FactParser fact = new FactParser();
+        private RuleParser rule = new RuleParser();
+
         public ESProvider(FactParser fact,RuleParser rule)
         {
             this.fact = fact;
             this.rule = rule;
+        }
+        public void CollectAnswer()
+        {
+
+        }
+        public bool GetAnswersByQuestion()
+        {
+            return false;
+        }
+        public string Evaluate()
+        {
+            return "LOL";
         }
     }
 }
