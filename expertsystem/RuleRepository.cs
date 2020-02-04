a4b805b285f0729c6bde6afd3f34de6f1cd296f8
Gergő <dgeri170@gmail.com>
2020-02-04 13:19:24 +0100

    faszom

---
+++ expertsystem/RuleRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace expertsystem
+{
+    class RuleRepository
+    {
+        public Dictionary<string, string> ruleRepo = new Dictionary<string, string>();
+    }
+}
