51ced3665b67e7040a069a3c9455a216843f129a
Gergő <dgeri170@gmail.com>
2020-02-04 13:17:39 +0100

    asd

--- expertsystem/XMLParser.cs
+++ expertsystem/XMLParser.cs
@@ -7,7 +7,44 @@ namespace expertsystem
 {
     abstract class XMLParser
     {
-        abstract public void loadXmlDocument(String XmlPath);
+        public static void loadXmlDocument(String XmlPath)
+        {
+            
+            XmlDocument doc = new XmlDocument();
+            doc.Load(XmlPath);
+
+            List<Fact> facts = new List<Fact>();
+            foreach(XmlNode factNode in doc.DocumentElement.ChildNodes)
+            {
+                string factId = factNode.Attributes["id"].Value;
+                foreach (XmlNode descNode in factNode.ChildNodes)
+                {
+                    string descValue = descNode.Attributes["value"].Value;
+                    Fact fact = new Fact(factId, descValue);
+                    facts.Add(fact);
+
+                    XmlNode evalsNode = descNode.FirstChild;
+                    ////foreach (XmlNode evalNode in evalsNode.ChildNodes)
+                    ////{
+                    //    string evalId = evalNode.Attributes["id"].Value;
+                    //    string evalValue = evalNode.InnerText;
+                    //    //fact.SetFactValueById(evalId, evalValue);
+                    //}
+
+
+                }
+            }
+            /*if (root.HasChildNodes)
+            {
+                for (int i = 0; i < root.ChildNodes.Count; i++)
+                {
+                    Console.WriteLine(root.ChildNodes[i].InnerText);
+                }
+            }*/
+            
+
+
+        }
         
 
         
